bc4928a848d3b76e181b50a026f309e4de72d835
Ghast <wolfsparkvanb@gmail.com>
2025-08-06 17:35:36 -0500

    some biteymcstabface attribution added

--- Content.Shared/Silicons/Borgs/Components/BorgChassisComponent.cs
+++ Content.Shared/Silicons/Borgs/Components/BorgChassisComponent.cs
@@ -7,7 +7,7 @@
 // SPDX-FileCopyrightText: 2024 deltanedas <39013340+deltanedas@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
-// SPDX-FileCopyrightText: 2025  <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2025 biteymcstabface
 //
 // SPDX-License-Identifier: MIT
 using Content.Shared.Alert;
